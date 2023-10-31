82572ef3e814a1a0a81880dd119f8f37e6f3daf4
Gustavo Mariano <gustavomarianosoares@gmail.com>
2023-10-31 16:11:06 -0300

    Registrar se finalizado

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import { 
+import React, { useState, useEffect } from "react";
+import {
   ScrollView,
   StyleSheet,
   Text,
@@ -9,17 +9,26 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
-  Alert
-} from 'react-native';
+  Alert,
+} from "react-native";
 
-import auth from '@react-native-firebase/auth';
+import Checkbox from "expo-checkbox";
 
+import auth from "@react-native-firebase/auth";
 
-export default function Register({navigation}) {
+export default function Register({ navigation }) {
+  const [text, setText] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confSenha, setConfSenha] = useState("");
+  const [isChecked, setChecked] = useState(false);
 
-  const [text, setText] = useState('');
-  const [senha, setSenha] = useState('');
-  const [confSenha, setConfSenha] = useState('');
+  function changePasswordSecure() {
+    if (isChecked) {
+      setChecked(false);
+    } else {
+      setChecked(true);
+    }
+  }
 
   function registerValidation() {
     if (text == "" || senha == "" || confSenha == "") {
@@ -42,8 +51,7 @@ export default function Register({navigation}) {
       .createUserWithEmailAndPassword(text, senha)
       .then(() => {
         auth().currentUser.sendEmailVerification();
-        auth().currentUser.updateProfile({
-        });
+        auth().currentUser.updateProfile({});
         Alert.alert(
           "CADASTRADO",
           "Usuário cadastrado no sistema com sucesso, enviamos um e-mail para que você verifique-o somente assim poderá entrar no sistema."
@@ -89,107 +97,143 @@ export default function Register({navigation}) {
     <KeyboardAvoidingView style={styles.background}>
       <View style={styles.containerLogo}>
         <Image
-          style={{width: 350, height: 210}}
-          source={require('../../assets/mycine.png')}
-        />     
+          style={{ width: 350, height: 210 }}
+          source={require("../../assets/mycine.png")}
+        />
       </View>
 
-      <View 
-        style={[
-          styles.container        
-        ]}
-      >
-        <TextInput 
-        style ={styles.input}
-        placeholder='Email'
-        autoCorrect={false}
-        onChangeText={newText => setText(newText)}
-        defaultValue={text}
+      <View style={[styles.container]}>
+        <TextInput
+          keyboardType="email-address"
+          autoCapitalize='none'
+          style={styles.input}
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={(newText) => setText(newText)}
+          defaultValue={text}
         />
 
-        <TextInput 
-        style ={styles.input}
-        placeholder='Senha'
-        autoCorrect={false}
-        onChangeText={newSenha => setSenha(newSenha)}
-        defaultValue={senha}
+        <TextInput
+          secureTextEntry={!isChecked}
+          style={styles.input}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={(newSenha) => setSenha(newSenha)}
+          defaultValue={senha}
         />
 
-        <TextInput 
-        style ={styles.input}
-        placeholder='Confirmar Senha'
-        autoCorrect={false}
-        onChangeText={newConfSenha => setConfSenha(newConfSenha)}
-        defaultValue={confSenha}
+        <TextInput
+          secureTextEntry={!isChecked}
+          style={styles.input}
+          placeholder="Confirmar Senha"
+          autoCorrect={false}
+          onChangeText={(newConfSenha) => setConfSenha(newConfSenha)}
+          defaultValue={confSenha}
         />
 
-        <TouchableOpacity 
-          disabled = { text.length > 8 && senha.length > 8 && confSenha.length > 8 && senha == confSenha ? false : true }
-          style={text.length > 8 && senha.length > 8 && confSenha.length > 8 && senha == confSenha ? styles.btnSubmit : styles.btnDisablade}
-          onPress={registerValidation}>
+        <TouchableOpacity
+          onPress={changePasswordSecure}
+          style={{
+            flexDirection: "row",
+            alignSelf: "flex-start",
+            marginBottom: 30,
+            marginLeft: 20,
+          }}
+        >
+          <Checkbox
+            value={isChecked}
+            color={isChecked ? "#339FFF" : "#797979"}
+          />
+
+          <Text style={{ color: "#fff", marginLeft: 5 }}>Mostrar senhas</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          disabled={
+            text.length > 8 &&
+            senha.length > 8 &&
+            confSenha.length > 8 &&
+            senha == confSenha
+              ? false
+              : true
+          }
+          style={
+            text.length > 8 &&
+            senha.length > 8 &&
+            confSenha.length > 8 &&
+            senha == confSenha
+              ? styles.btnSubmit
+              : styles.btnDisablade
+          }
+          onPress={registerValidation}
+        >
           <Text style={styles.submitText}>Registrar-se</Text>
         </TouchableOpacity>
 
-      </View>    
+        <TouchableOpacity
+          style={{ marginTop: 30 }}
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text style={{ color: "#fff" }}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  background:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    backgroundColor: '#191a30'
+  background: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#191a30",
   },
-  containerLogo:{
-    flex:1,
-    justifyContent:'center',
-    paddingTop:50
+  containerLogo: {
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: 50,
   },
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center',
-    width:'90%',
-    paddingBottom:260
+  container: {
+    flex: 1,
+    alignItems: "center",
+    width: "90%",
+    paddingBottom: 260,
   },
-  input:{
-    backgroundColor:'#fff',
-    width:'90%',
-    marginBottom:15,
-    color:'#222',
+  input: {
+    backgroundColor: "#fff",
+    width: "90%",
+    marginBottom: 15,
+    color: "#222",
     fontSize: 17,
     borderRadius: 10,
-    padding:12
+    padding: 12,
   },
-  btnSubmit:{
-    backgroundColor:'#e72f49',
-    width:'90%',
-    height:45,
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius:10,
+  btnSubmit: {
+    backgroundColor: "#e72f49",
+    width: "90%",
+    height: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
   },
-  btnDisablade:{
-    backgroundColor:'#e72f49',
-    width:'90%',
-    height:45,
-    alignItems:'center',
-    justifyContent:'center',
-    borderRadius:10,
-    opacity: 0.5
+  btnDisablade: {
+    backgroundColor: "#e72f49",
+    width: "90%",
+    height: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    opacity: 0.5,
   },
-  submitText:{
-    color:'#fff',
-    fontSize: 18
+  submitText: {
+    color: "#fff",
+    fontSize: 18,
   },
-  btnRegister:{
+  btnRegister: {
     marginTop: 10,
   },
-  RegisterText:{
-    color:'#fff',
-    fontSize: 15
-  }
-
+  RegisterText: {
+    color: "#fff",
+    fontSize: 15,
+  },
 });
